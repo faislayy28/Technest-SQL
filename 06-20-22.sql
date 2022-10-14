4d076340d025ddccba0ae60ab07f79038a27741a
faislayy28 <63189481+faislayy28@users.noreply.github.com>
2022-10-13 22:34:59 -0500

    Add files via upload
    CREATE
    INSERT
    SELECT

---
+++ 06-20-22.sql
@@ -0,0 +1,42 @@
+REM   Script: 06-20-22
+REM   CREATE
+INSERT
+SELECT
+
+
+CREATE TABLE Employee(Emp_id int,Emp_name Varchar(50),Salary int,Location Varchar(50),Dept Varchar(50));
+
+Desc Employee
+
+
+CREATE TABLE Employee(Emp_id int,Emp_name Varchar(50),Salary int,Location Varchar(50),Dept Varchar(50)) 
+;
+
+Drop table Employee;
+
+CREATE TABLE Employee(Emp_id int,Emp_name Varchar(50),Salary int,Location Varchar(50),Dept Varchar(50)) 
+;
+
+CREATE TABLE Employee(Emp_id int,Emp_name Varchar(50),Salary int,Location Varchar(50),Dept Varchar(50)) 
+Desc Table;
+
+Desc Table
+
+
+Desc Employee
+
+
+INSERT INTO Employee VALUES (1, 'A', 100, 'Delhi', 'Tech');
+
+SELECT * FROM Employee;
+
+INSERT INTO Employee VALUES (2, 'B', 200, 'Chennai', 'Sales');
+
+INSERT INTO Employee VALUES (2, 'B', 200, 'Chennai', 'Sales');
+
+INSERT INTO Employee VALUES (3, 'C', 300, 'Bangalore', 'Tech dept 123');
+
+INSERT INTO Employee VALUES (4, 'D', 400, 'Hyd', 12345);
+
+INSERT INTO Employee Values(5, 'E', 500, 'Mumbai', 'HR');
+
